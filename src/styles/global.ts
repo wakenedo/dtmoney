7ff60f0906e7da7da59a1f99b4e030a713e26616
wakenedo <a_nuner@outlook.com>
2021-11-16 10:51:27 -0300

    Styling Part 4 - Dashboard Component
    Dashboard:
    Summary component Added (Static)

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         --background: #F0F2F5;
         --red: #E62E4D;
         --blue: #5429CC;
+        --green: #33CC95;
 
         --blue-light: #6933FF;
         
